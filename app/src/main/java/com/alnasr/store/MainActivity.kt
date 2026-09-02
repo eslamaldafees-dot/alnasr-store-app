63f23ae8163555587c868f540d5701b35b792c5f
eslamaldafees-dot <eslamaldafees@gmail.com>
2026-09-02 12:03:27 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/alnasr/store/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.alnasr.store
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val text = TextView(this)
+        text.text = "متجر النصر"
+        text.textSize = 32f
+
+        setContentView(text)
+    }
+}
